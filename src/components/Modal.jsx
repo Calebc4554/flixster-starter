830c591236971ebfa8306edcf575fc491eeffddd
Caleb Calderon <calebc4554@meta.com>
2025-06-13 17:07:47 -0700

    removed duplicate, repeating, and overwriting rules in css and cleaned it up

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ show, onClose, children }) => {
     }
     return (
         <div className='modalOverlay'>
-            <div className='modalContent'>
+            <div className='modalContentContainer'>
                 <button type='button' className='modalClose' onClick={onClose}>&times;</button>
                 {children}
             </div>
